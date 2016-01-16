80fe5d76100f9f3a5db02f3ff481272781f581e3
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2016-01-16 20:10:19 +0100

    Update deps.sh

--- tools/travis/deps.sh
+++ tools/travis/deps.sh
@@ -9,10 +9,12 @@ case "$TRAVIS_OS_NAME" in
   linux)
     wget https://www.dropbox.com/s/lavj7413vfgrign/vtk_6.3-1_amd64.deb?dl=1 -O vtk.deb
     sudo dpkg -i vtk.deb
+    sudo apt-get install -y libginac-dev
   ;;
   osx)
     set +e
     brew install homebrew/science/vtk
+    brew install ginac
     set -e
   ;;
 esac
